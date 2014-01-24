75f89a4b302b28744820e7d42481b1c68ee8620f
John-David Dalton <john.david.dalton@gmail.com>
2014-01-24 09:23:26 -0800

    Update vendor/qunit-extras and simplify its load/install.

--- test/test.js
+++ test/test.js
@@ -91,11 +91,16 @@
       root.addEventListener || (root.addEventListener = noop),
       root.setTimeout || (root.setTimeout = noop),
       root.QUnit = load('../vendor/qunit/qunit/qunit.js') || root.QUnit,
-      (load('../vendor/qunit-extras/qunit-extras.js') || { 'runInContext': noop }).runInContext(root),
       addEventListener === noop && delete root.addEventListener,
       root.QUnit
     );
   }());
+
+  /** Load and install QUnit Extras */
+  if (load) {
+    var qa = load('../vendor/qunit-extras/qunit-extras.js');
+    qa.runInContext(root);
+  }
 
   /*--------------------------------------------------------------------------*/
 
